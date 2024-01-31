dceff76619b3c8c292f03b5c7ddc4120608a4e8f
Shouldgooo <84283235+Shouldgooo@users.noreply.github.com>
2024-01-31 13:13:21 +1100

    Lecture1

---
+++ Jiuzhang/Lecture1/Lecture1/src/com/jiuzhang/Main.java
@@ -0,0 +1,12 @@
+package com.jiuzhang;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int num = 10;
+        int num2 = num + 3;
+        int num3 = 123 % 10;
+        System.out.println(num3);
+
+    }
+}
